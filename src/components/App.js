a926a26698993be5c6c4a3b718c593742a52b981
DPS <smith.dp@gmail.com>
2020-01-02 16:01:14 +0000

    new app component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Material UI Playground with CRA</h1>
+    </div>
+  );
+}
+
+export default App;
