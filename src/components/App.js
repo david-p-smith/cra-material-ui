8bb246ee2c53ea031968e688c17f6a79fcadccf2
DPS <smith.dp@gmail.com>
2020-01-02 16:57:32 +0000

    add footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Header from "./fragments/Header";
+import Footer from "./fragments/Footer";
 import Nav from "./fragments/Nav";
 import HomePage from "./pages/Home";
 import ButtonsPage from "./pages/Buttons";
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/buttons" component={ButtonsPage} />
       </Switch>
+      <Footer />
     </>
   );
 }
---
+++ src/components/fragments/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Site by David. &copy; 2020 David Smith.</p>
+    </footer>
+  );
+}
+
+export default Footer;
