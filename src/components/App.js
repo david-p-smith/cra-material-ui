1474ce28923d40e74037c137518633d09d35e55e
DPS <smith.dp@gmail.com>
2020-01-02 16:28:56 +0000

    add header

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Header from "./fragments/Header";
 import HomePage from "./pages/Home";
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <Header />
       <div>
-        <h1>Material UI Playground with CRA</h1>
         <Button variant="contained" color="primary">
           Hello World!
         </Button>
