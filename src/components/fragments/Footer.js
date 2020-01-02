d3c857c538059715e7d2c7a6250f76ca0519bc3b
DPS <smith.dp@gmail.com>
2020-01-02 17:02:46 +0000

    style the footer

--- src/components/fragments/Footer.js
+++ src/components/fragments/Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    top: "auto",
+    bottom: 0,
+    textAlign: "center"
+  }
+}));
 
 function Footer() {
+  const classes = useStyles();
+
   return (
     <footer>
-      <p>Site by David. &copy; 2020 David Smith.</p>
+      <AppBar className={classes.appBar}>
+        <p>Site by David. &copy; 2020 David Smith.</p>
+      </AppBar>
     </footer>
   );
 }
