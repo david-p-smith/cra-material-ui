c2cef3d68c1db6762549fd11d61b2271d02569b9
DPS <smith.dp@gmail.com>
2020-01-02 16:07:51 +0000

    add mui hello world button

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+// import { Route, Switch } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div>
       <h1>Material UI Playground with CRA</h1>
+      <Button variant="contained" color="primary">
+        Hello World!
+      </Button>
     </div>
   );
 }
