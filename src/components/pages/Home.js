006a9a81080f85afa1a14c3bcbcfb9d361035fb6
DPS <smith.dp@gmail.com>
2020-01-02 16:55:48 +0000

    add some products

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { Grid, Paper } from "@material-ui/core";
 
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Welcome to the app!</h1>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <Paper>
+            <h1>Product 1</h1>
+            <p>Product blurb here</p>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <h1>Product 1</h1>
+          <p>Product blurb here</p>
+        </Grid>
+        <Grid item xs={4}>
+          <h1>Product 1</h1>
+          <p>
+            Product blurb here. Product blurb here. Product blurb here. Product
+            blurb here. Product blurb here. Product blurb here. Product blurb
+            here. Product blurb here. Product blurb here. Product blurb here.
+            Product blurb here. Product blurb here. Product blurb here. Product
+            blurb here. Product blurb here. Product blurb here. Product blurb
+            here. Product blurb here. Product blurb here. Product blurb here.
+            Product blurb here. Product blurb here. Product blurb here. Product
+            blurb here. Product blurb here. Product blurb here. Product blurb
+            here. Product blurb here. Product blurb here. Product blurb here.
+            Product blurb here. Product blurb here. Product blurb here. Product
+            blurb here. Product blurb here. Product blurb here. Product blurb
+            here.{" "}
+          </p>
+        </Grid>
+      </Grid>
     </div>
   );
 }
