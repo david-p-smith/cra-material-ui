ccabd58b854433e10dec0dcbd7073c7238d71c37
DPS <smith.dp@gmail.com>
2020-01-02 17:23:02 +0000

    upgrade styling on smaller screens

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,32 +6,37 @@ function Home() {
     <div>
       <h1>Welcome to the app!</h1>
       <Grid container spacing={3}>
-        <Grid item xs={4}>
+        <Grid item xs={12} md={4}>
           <Paper>
             <h1>Product 1</h1>
             <p>Product blurb here</p>
           </Paper>
         </Grid>
-        <Grid item xs={4}>
-          <h1>Product 1</h1>
-          <p>Product blurb here</p>
+        <Grid item xs={12} md={4}>
+          <Paper>
+            <h1>Product 1</h1>
+            <p>Product blurb here</p>
+          </Paper>
         </Grid>
-        <Grid item xs={4}>
-          <h1>Product 1</h1>
-          <p>
-            Product blurb here. Product blurb here. Product blurb here. Product
-            blurb here. Product blurb here. Product blurb here. Product blurb
-            here. Product blurb here. Product blurb here. Product blurb here.
-            Product blurb here. Product blurb here. Product blurb here. Product
-            blurb here. Product blurb here. Product blurb here. Product blurb
-            here. Product blurb here. Product blurb here. Product blurb here.
-            Product blurb here. Product blurb here. Product blurb here. Product
-            blurb here. Product blurb here. Product blurb here. Product blurb
-            here. Product blurb here. Product blurb here. Product blurb here.
-            Product blurb here. Product blurb here. Product blurb here. Product
-            blurb here. Product blurb here. Product blurb here. Product blurb
-            here.{" "}
-          </p>
+        <Grid item xs={12} md={4}>
+          <Paper>
+            <h1>Product 1</h1>
+            <p>
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here. Product blurb here. Product blurb here.
+              Product blurb here.{" "}
+            </p>
+          </Paper>
         </Grid>
       </Grid>
     </div>
