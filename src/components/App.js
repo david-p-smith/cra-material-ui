9c312b3fea22e82eb8fa4e6665c3f8db1b8b387a
DPS <smith.dp@gmail.com>
2020-01-02 17:18:49 +0000

    nav style update

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { Container } from "@material-ui/core";
 import Header from "./fragments/Header";
 import Footer from "./fragments/Footer";
 import Nav from "./fragments/Nav";
@@ -11,13 +12,16 @@ function App() {
   return (
     <>
       <CssBaseline />
+
       <Header />
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/buttons" component={ButtonsPage} />
-      </Switch>
-      <Footer />
+      <Container maxWidth="xl">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/buttons" component={ButtonsPage} />
+        </Switch>
+        <Footer />
+      </Container>
     </>
   );
 }
--- src/components/fragments/Nav.js
+++ src/components/fragments/Nav.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Button } from "@material-ui/core";
+import { Button, Toolbar } from "@material-ui/core";
 
 function Nav() {
   return (
-    <nav>
+    <Toolbar>
       <Button>
         <NavLink to="/">Home</NavLink>
       </Button>
       <Button>
         <NavLink to="/buttons">Buttons</NavLink>
       </Button>
-    </nav>
+    </Toolbar>
   );
 }
 
