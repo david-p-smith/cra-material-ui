c6e51e83b9077193e6932f711ef57d82f8fe66ab
DPS <smith.dp@gmail.com>
2020-01-02 16:13:29 +0000

    add baseline

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 // import { Route, Switch } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
   return (
-    <div>
-      <h1>Material UI Playground with CRA</h1>
-      <Button variant="contained" color="primary">
-        Hello World!
-      </Button>
-    </div>
+    <>
+      <CssBaseline />
+      <div>
+        <h1>Material UI Playground with CRA</h1>
+        <Button variant="contained" color="primary">
+          Hello World!
+        </Button>
+      </div>
+    </>
   );
 }
 
